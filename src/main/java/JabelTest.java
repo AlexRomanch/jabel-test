b96edb2e3439ae5afe2b67a9ad8ccbec55f97b40
romanchukas <romanchukas@nspk.ru>
2020-10-16 19:00:00 +0300

    initial commit

---
+++ src/main/java/JabelTest.java
@@ -0,0 +1,9 @@
+/*
+    @author romanchukas, 16.10.2020
+*/
+public class JabelTest {
+    public static void main(String[] args) {
+        var testVar = "here is var!";
+        System.out.println(testVar);
+    }
+}
